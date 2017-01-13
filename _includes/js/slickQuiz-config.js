4b86b9921c0706e55bc2ebde73ceda0bc7d56622
Abdul Hadi Alaidi <abd_ul_hadi@outlook.com>
2017-01-13 14:34:53 +0300

    Update Quiz1

--- _includes/js/slickQuiz-config.js
+++ _includes/js/slickQuiz-config.js
@@ -17,69 +17,112 @@ var quizJSON = {
         { // Question 1 - Multiple Choice, Single True Answer
             "q": "Convert (0.625) to binary  ()2",
             "a": [
-                {"option": "0.10010",      "correct": false},
-                {"option": "0.10110",     "correct": false},
-                {"option": "0.1001",      "correct": true},
+                {"option": "0.101",      "correct": true},
+                {"option": "0.1011",     "correct": false},
+                {"option": "0.1001",      "correct": false},
                 {"option": "1.1001",     "correct": false} // no comma here
             ],
             "correct": "<p><span>That's right!</span> </p>",
             "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
         },
-        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
+         { // Question 2 - Multiple Choice, Single True Answer
             "q": "Convert (0.375) to binary  ()2",
             "a": [
-                {"option": "1.1001",               "correct": false},
-                {"option": "1.1001",   "correct": true},
-                {"option": "1.1001",               "correct": false},
-                {"option": "1.1001", "correct": true} // no comma here
+                {"option": "0.01101",      "correct": false},
+                {"option": "0.011",     "correct": true},
+                {"option": "0.1001",      "correct": false},
+                {"option": "1.1101",     "correct": false} // no comma here
             ],
-            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+            "correct": "<p><span>That's right!</span> </p>",
+            "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
         },
-         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "Convert (0.625) to binary  ()2",
+         { // Question 3 - Multiple Choice, Single True Answer
+            "q": "Convert (6.875) to binary  ()2",
             "a": [
-                {"option": "0.10010",      "correct": false},
-                {"option": "0.10110",     "correct": false},
-                {"option": "0.1001",      "correct": true},
+                {"option": "100.101",      "correct": false},
+                {"option": "111.111",     "correct": false},
+                {"option": "110.11",      "correct": false},
+                {"option": "110.111",     "correct": true} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> </p>",
+            "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
+        },
+         { // Question 4 - Multiple Choice, Single True Answer
+            "q": "Convert (37) to octal number()8",
+            "a": [
+                {"option": "46",      "correct": false},
+                {"option": "45",     "correct": true},
+                {"option": "49",      "correct": false},
                 {"option": "1.1001",     "correct": false} // no comma here
             ],
             "correct": "<p><span>That's right!</span> </p>",
             "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
         },
-        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "Convert (0.375) to binary  ()2",
+         { // Question 5 - Multiple Choice, Single True Answer
+            "q": "Convert (548.3125) to binary  ()8",
             "a": [
-                {"option": "1.1001",               "correct": false},
-                {"option": "1.1001",   "correct": true},
-                {"option": "1.1001",               "correct": false},
-                {"option": "1.1001", "correct": true} // no comma here
-            ],
-            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
-        },
-         { // Question 1 - Multiple Choice, Single True Answer
-            "q": "Convert (0.625) to binary  ()2",
-            "a": [
-                {"option": "0.10010",      "correct": false},
-                {"option": "0.10110",     "correct": false},
-                {"option": "0.1001",      "correct": true},
-                {"option": "1.1001",     "correct": false} // no comma here
+                {"option": "1044.24",      "correct": true},
+                {"option": "1144.24",     "correct": false},
+                {"option": "1045.24",      "correct": false},
+                {"option": "1044.34",     "correct": false} // no comma here
             ],
             "correct": "<p><span>That's right!</span> </p>",
             "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
         },
-        { // Question 2 - Multiple Choice, Multiple True Answers, Select Any
-            "q": "Convert (0.375) to binary  ()2",
+         { // Question 6 - Multiple Choice, Single True Answer
+            "q": "Convert (95) to hexadecimal ()16",
             "a": [
-                {"option": "1.1001",               "correct": false},
-                {"option": "1.1001",   "correct": true},
-                {"option": "1.1001",               "correct": false},
-                {"option": "1.1001", "correct": true} // no comma here
+                {"option": "5A",      "correct": false},
+                {"option": "5B",     "correct": false},
+                {"option": "6F",      "correct": false},
+                {"option": "5F",     "correct": true} // no comma here
             ],
-           // "select_any": true,
-            "correct": "<p><span>Nice!</span> Your cholestoral level is probably doing alright.</p>",
-            "incorrect": "<p><span>Hmmm.</span> You might want to reconsider your options.</p>" // no comma here
+            "correct": "<p><span>That's right!</span> </p>",
+            "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
+        },
+         { // Question 7 - Multiple Choice, Single True Answer
+            "q": "Convert (5213.879) to hexadecimal ()16",
+            "a": [
+                {"option": "145D.E106",      "correct": true},
+                {"option": "245D.F106",     "correct": false},
+                {"option": "165D.A106",      "correct": false},
+                {"option": "146D.C106",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> </p>",
+            "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
+        },
+         { // Question 8 - Multiple Choice, Single True Answer
+            "q": "Convert (110101)2 to decimal ().",
+            "a": [
+                {"option": "53",      "correct": true},
+                {"option": "56",     "correct": false},
+                {"option": "54",      "correct": false},
+                {"option": "73",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> </p>",
+            "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
+        },
+         { // Question 9 - Multiple Choice, Single True Answer
+            "q": "Convert (1101.01)2 to decimal ().",
+            "a": [
+                {"option": "0.25",      "correct": false},
+                {"option": "0.26",     "correct": false},
+                {"option": "13.25",      "correct": true},
+                {"option": "13",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> </p>",
+            "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
+        },
+         { // Question 0 - Multiple Choice, Single True Answer
+            "q": "Convert (B65F)16 to decimal ().",
+            "a": [
+                {"option": "46681",      "correct": false},
+                {"option": "56687",     "correct": false},
+                {"option": "46687",      "correct": true},
+                {"option": "66687",     "correct": false} // no comma here
+            ],
+            "correct": "<p><span>That's right!</span> </p>",
+            "incorrect": "<p><span>Uhh no.</span> <em>go</em> </p>" // no comma here
         }
     ]
 };
